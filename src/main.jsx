7eb36b5db68b19f2f604c14d8091b5fda8cab0f7
hashirkhanfr <hashirkhanfr@gmail.com>
2025-06-30 11:27:10 +0500

    Import HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import { CssBaseline } from '@mui/material';
+import { HashRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
